053c995d01efa86862c20e7a2df65599caa81e8a
Jayson Garcia <53173775+hallowichig0@users.noreply.github.com>
2023-01-03 00:20:28 +0800

    Fixed Unmatched '}' error

--- remove-customizer-menu.php
+++ remove-customizer-menu.php
@@ -8,7 +8,7 @@
  * Author URI: https://hallowichig0.github.io
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
-*
+*/
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
